5015b64228d7dd6c8ddc5bf3ed12fba9e8b88481
Katerina Slavova <katerina.slavova@mentormate.com>
2019-07-19 16:47:47 +0300

    Test OOP

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -12,10 +12,28 @@ namespace FirstProject
             User userB = new User();
 
             userA.Username = "test.user";
-            Console.WriteLine("Username A: " + userA.Username);
+            /*Console.WriteLine("Username A: " + userA.Username);
             Console.WriteLine("Username B: " + userB.Username);
             Console.WriteLine("Role A: " + userA.Role);
-            Console.WriteLine("Role B: " + userB.Role);
+            Console.WriteLine("Role B: " + userB.Role);*/
+
+            userA.FirstNAme = "First Name A";
+            userA.LastName = "Last Name A";
+
+            userB.FirstNAme = "First Name B";
+            userB.LastName = "Last Name B";
+
+
+            userA.Password = "123";
+            userA.Password = "321";
+            
+
+            userA.Age = 21;
+
+            userA.ChangePassword("123", "1234");
+            userA.ChangePassword("123", "1234");
+
+
 
         }
     }
--- FirstProject/User.cs
+++ FirstProject/User.cs
@@ -10,6 +10,48 @@ namespace FirstProject
     {
         private string username;
         private string role = "Regular user";
+        private string firstName;
+        private string lastName;
+        private string password;
+       
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+
+        public string FirstNAme
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string Fullname
+        {
+            get
+            {
+                return this.firstName + " " + this.lastName;
+            }
+        }
+
+        public string Password
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(password))
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Password for {0}has already been set. Please use ChangePassword() instead.", this.Username);
+                }
+            }
+        }
+
+
 
         public string Username
         {
@@ -24,5 +66,50 @@ namespace FirstProject
             set { role = value; }
 
         }
-    }
+
+        public int Age { get; set; }
+
+        public bool ChangePassword(string currentPassword, string newPassword)
+        {
+            bool passwordHasBeenChanged = false;
+
+            if (string.IsNullOrEmpty(this.password))
+            {
+                Console.WriteLine("Password for {0} has not been set yet.", this.Username);
+            }
+            else if (this.password != currentPassword)
+            {
+                Console.WriteLine("Invalid current password for " + this.Username);
+            }
+            else
+            {
+                this.password = newPassword;
+                Console.WriteLine("Successfully changed password for "+this.Username);
+            }
+
+            return passwordHasBeenChanged;
+        }
+
+        public void ViewProfile()
+        {
+            Console.WriteLine("Full name: " + this.Fullname);
+            Console.WriteLine("Age: " + this.Age);
+            Console.WriteLine("Username: " + this.Username);
+            Console.WriteLine("Role: " + this.Role);
+            Console.WriteLine();
+        }
+
+        public void ViewMainMenu()
+        {
+            Console.WriteLine("Logout");
+            Console.WriteLine("Home");
+            Console.WriteLine("Tasks");
+            Console.WriteLine("View profile");
+            Console.WriteLine();
+        }
+
+
+    
+            
+     }
 }
